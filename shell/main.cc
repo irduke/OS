d8b64c56f836d50ed5d625508c88adee1b629587
Isaac Duke <ird2dpj@virginia.edu>
2022-09-07 17:35:04 -0400

    WIP

--- shell/main.cc
+++ shell/main.cc
@@ -9,6 +9,33 @@
 #include <unistd.h>
 #include <vector>
 #include <sstream>  
+#include <queue>
+
+#define MAX_NUM_TOKENS 50
+#define DEBUG false
+
+typedef struct cmd
+{
+    const char * args[MAX_NUM_TOKENS];
+    bool redirect;
+    int arg_index;
+    const char * func;
+} cmd;
+
+void init_cmd(cmd * command);
+
+void parse_and_run_command(const std::string &command);
+
+
+int main(void) {
+    std::string command;
+    std::cout << "> ";
+    while (std::getline(std::cin, command)) {
+        parse_and_run_command(command);
+        std::cout << "> ";
+    }
+    return 0;
+}
 
 void parse_and_run_command(const std::string &command) {
     /* TODO: Implement this. */
@@ -20,63 +47,88 @@ void parse_and_run_command(const std::string &command) {
         exit(0);
     }
 
-    // split string into individual commands & tokens
-    std::vector<std::vector<std::string>> tokens;
     std::stringstream s(command);
-    std::string temp;
-    int numCommands = 1;
-    std::vector<std::string> firstVec;
-    tokens.push_back(firstVec);
-    while(std::getline(s, temp, ' ')) {
-        if(temp == "|") {
-            numCommands++;
-            std::vector<std::string> tempVec;
-            tokens.push_back(tempVec);
-        }
-        else {
-            tokens[numCommands-1].push_back(temp);
+    std::queue<std::string> tokens;
+    std::queue<cmd> command_queue;
+    std::string t;
 
+    while(s >> t) {
+        tokens.push(t);
+    }
+
+    if (DEBUG) {
+        std::cout << "Tokens: ";
+        std::queue<std::string> t_cpy = tokens;
+        while (!t_cpy.empty()) {
+            std::cout << t_cpy.front() << " ";
+            t_cpy.pop();
+        }
+        std::cout << std::endl;
+    }
+
+    std::string curr_token;
+    int num_tokens = tokens.size();
+    int token_counter = 0;
+
+    while (!tokens.empty()) {
+        //Create and init command struct
+        cmd * curr_command;
+        init_cmd(&curr_command);
+        //Grab function to execute and pop from queue
+        curr_command.func = tokens.front().c_str();
+        tokens.pop();
+        while (curr_token != "|" && token_counter < num_tokens - 1) {
+            curr_token = tokens.front();
+            curr_command.args[curr_command.arg_index] = tokens.front().c_str();
+            curr_command.arg_index++;
+            token_counter++;
+            tokens.pop();
+        }
+        curr_command.args[curr_command.arg_index] = NULL;
+        command_queue.push(curr_command);
+    }
+
+    if (DEBUG) {
+        std::queue<cmd> cmd_cpy = command_queue;
+        while (!cmd_cpy.empty()) {
+            std::cout << "Command: ";
+            cmd front = cmd_cpy.front();
+            std::cout << "num_args=" << front.arg_index << " " << front.func << " ";
+            for (int i = 0; i <= front.arg_index; i++) {
+                std::cout << front.args[i] << " ";
+            }
+            cmd_cpy.pop();
+            std::cout << std::endl;
         }
     }
 
-    for(int i = 0; i < numCommands; i++) {
-        std::vector<std::string> command = tokens[i];
-        int status;
-        
-        std::string func = command[0];
-        const char *args;
-        if(command.size() == 1) {
-            args = (char *)0;
-        }
-        else {
-            args = command[1].c_str();
-        }
+
+    while (!command_queue.empty()) {
+        int status = 0;
+        cmd run_command = command_queue.front();
+        command_queue.pop();
 
         pid_t pid = fork();
         if(pid == 0) {
-            const char * exec_file = func.c_str();
-            execlp(exec_file, exec_file, args, (char *)NULL);
+            //Child
+            execvp(run_command.func, (char **) run_command.args);
+            perror("execvp");
+            exit(errno);
         }
         else if(pid > 0) {
+            //Parent process, wait for child
             waitpid(pid, &status, 0);
         }
         else {
-            std::cout << "this was an error";
+            std::cerr << "Fork" << std::endl;
         }
 
-        if(WIFEXITED(status)) {
-            std::cerr << func << " exit status: " << WEXITSTATUS(status) << std::endl;
-        }
+        std::cout << run_command.func << " exit status: " << WEXITSTATUS(status) << std::endl;
     }
-    
 }
 
-int main(void) {
-    std::string command;
-    std::cout << "> ";
-    while (std::getline(std::cin, command)) {
-        parse_and_run_command(command);
-        std::cout << "> ";
-    }
-    return 0;
+void init_cmd(cmd * command) {
+    memset(command->args, '\0' , sizeof(command->args));
+    command->redirect = false;
+    command->arg_index = 0;
 }
