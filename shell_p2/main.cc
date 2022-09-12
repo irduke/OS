78f52b23dee9f7164fd8f2fb2616b2527feb3f5a
Isaac Duke <ird2dpj@virginia.edu>
2022-09-12 18:54:12 -0400

    Start of shell p2

--- shell_p2/main.cc
+++ shell_p2/main.cc
@@ -19,10 +19,22 @@
 typedef struct cmd
 {
     std::vector<char*> args;
-    bool redirect;
     int arg_index;
     const char * func;
+    const char * redir_out;
+    const char * redir_in;
 } cmd;
+
+std::ostream& operator<<(std::ostream& os, const cmd& command) {
+    size_t i;
+    os << "Command args: ";
+    for (i=0; i<command.args.size(); i++) {
+        os << command.args[i] << " ";
+    }
+    os << "Redir output: " << command.redir_out << ", ";
+    os << "Redir input: " << command.redir_in << ", ";
+    return os;
+}
 
 void init_cmd(cmd * command);
 
@@ -78,12 +90,30 @@ void parse_and_run_command(const std::string &command) {
         curr_command.func = const_cast<char*>(tokens.at(token_counter).c_str());
         while (token_counter < num_tokens && tokens.at(token_counter) != "|") {
             curr_token = tokens.at(token_counter);
-            curr_command.args.push_back(const_cast<char*>(tokens.at(token_counter).c_str()));
-            curr_command.arg_index++;
-            token_counter++;
+            if (curr_token == ">") {
+                //Skip over redirect token
+                token_counter++;
+                //set current command output to redirect output
+                curr_command.redir_out = const_cast<char*>(tokens.at(token_counter).c_str());
+                token_counter++;
+            }
+            else if (curr_token == "<") {
+                //Skip over redirect token
+                token_counter++;
+                //set current command input to redirect input
+                curr_command.redir_in = const_cast<char*>(tokens.at(token_counter).c_str());
+                token_counter++;
+            }
+            else {
+                curr_command.args.push_back(const_cast<char*>(tokens.at(token_counter).c_str()));
+                curr_command.arg_index++;
+                token_counter++;
+            }
         }
-        curr_command.args.push_back(NULL);
-        command_queue.push(curr_command);
+        if(is_well_formed(curr_command.args)) {
+            curr_command.args.push_back(NULL);
+            command_queue.push(curr_command);
+        }
     }
 
     while (!command_queue.empty()) {
@@ -114,7 +144,39 @@ void parse_and_run_command(const std::string &command) {
 
 void init_cmd(cmd * command) {
     //memset(command->args, '\0' , sizeof(command->args));
-    command->redirect = false;
     command->arg_index = 0;
 }
 
+bool is_word(std::string token) {
+    // Uses regex to check if a string is alphabetical
+    return std::regex_match(token, std::regex("^[A-Za-z/]+$"));
+}
+
+bool is_well_formed(std::vector<char*> command) {
+    /*
+    Checks that each command meets the following conditions...
+    1) No more than one input redirection
+    2) No more than one output redirection
+    3) At least one or more words
+    */
+    bool is_well_formed = true;
+    int num_input_redirect = 0;
+    int num_output_redirect = 0;
+    int num_words = 0;    
+
+    for(size_t i = 0; i < command.size(); i++) {
+        if(i < command.size()-1) {
+            // verify redirect character & word token combination
+            if((strcmp(command.at(i), "<") == 0) && is_word(command.at(i+1))) num_input_redirect++;
+            else if((strcmp(command.at(i), ">") == 0) && is_word(command.at(i+1))) num_output_redirect++;
+        }
+        if(is_word(command.at(i))) num_words++;
+    }
+
+    if(num_input_redirect > 1 || num_output_redirect > 1 || num_words < 1) {
+        return !is_well_formed;
+    }
+    else {
+        return is_well_formed;
+    }
+}
