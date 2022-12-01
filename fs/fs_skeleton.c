1634d1c1ce940a91e4542036724b2adaaad0d5eb
sarahgould <sarah@igould.com>
2022-12-01 13:28:47 -0500

    added some more changes

--- fs/fs_skeleton.c
+++ fs/fs_skeleton.c
@@ -6,15 +6,16 @@
 #include "inode.h"
 
 #define TOTAL_BLOCKS (10*1024)
+  // TODO: redefine total blocks so it can be dynamically allocated by nblocks
 
-static unsigned char rawdata[TOTAL_BLOCKS*BLOCK_SZ];
-static char bitmap[TOTAL_BLOCKS];
+static unsigned char *rawdata;
+static char *bitmap;
 
 int get_free_block()
 {
   // fill in here
   int blockno;
-  for(blockno = 0; blockno < TOTAL_BLOCKS; blockno+=1024) {
+  for(blockno = 0; blockno < TOTAL_BLOCKS; blockno++) {
     if(bitmap[blockno]) continue;
     else return blockno;
   }
@@ -31,7 +32,7 @@ void write_int(int pos, int val)
 
 void place_file(char *file, int uid, int gid)
 {
-  int i, j, nbytes = 0;
+  int i, j, n, nbytes = 0;
   int i2block_index, i3block_index;
   struct inode *ip;
   FILE *fpr;
@@ -55,14 +56,16 @@ void place_file(char *file, int uid, int gid)
     int blockno = get_free_block();
     ip->dblocks[i] = blockno;
     // fill in here
-    // fread(ip->dblocks[i], 1024, 1, fpr);
+    n = fread(&rawdata[ip->dblocks[i]*1024], 1024, 1, fpr);
+    if(n < 1024) return;
   }
 
   // fill in here if IBLOCKS needed
   // if so, you will first need to get an empty block to use for your IBLOCK
   for (i = 0; i < N_IBLOCKS; i++){
     for(j = 0; j < N_DBLOCKS; j++) {
-      // fill in
+      n = fread(&rawdata[rawdata[ip->iblocks[i]*1024]*1024], 1024, 1, fpr);
+      if(n < 1024) return;
     }
   }
 
@@ -74,6 +77,21 @@ void main(int argc, char *argv[]) // add argument handling
 {
   int i;
   FILE *outfile;
+  int nblocks, iblocks, uid, gid, inodepos; // create
+  
+  // TODO: fix argument handling
+
+  // create
+  if(argv[0] == "-create") {
+    nblocks = atoi(argv[4]);
+    iblocks = atoi(argv[6]);
+    uid = atoi(argv[10]);
+    gid = atoi(argv[12]);
+    inodepos = atoi(argv[14]);
+  }
+
+  rawdata = (unsigned char*)malloc(nblocks*BLOCK_SZ);
+  bitmap = (char*)malloc(nblocks);
 
   outfile = fopen(output_filename, "wb");
   if (!outfile) {
